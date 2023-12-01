589cdc8db1287762d46bd9216f4d5d97b6d23344
Vojtěch Šanda <vojtech.sanda@gmail.com>
2023-12-01 08:22:24 +0100

    chore: Day 1 init

---
+++ solutions/01/input.js
@@ -0,0 +1,8 @@
+import fs from 'fs';
+import path from 'path';
+
+export const getExampleInput = async () =>
+  fs.readFileSync(path.resolve(process.env.NODE_PATH, 'inputs/01/example.txt')).toString();
+
+export const getInput = async () =>
+  fs.readFileSync(path.resolve(process.env.NODE_PATH, 'inputs/01/input.txt')).toString();
